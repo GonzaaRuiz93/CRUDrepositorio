185ad1985a2887fdfdecd673c12f49a0a58e9877
Gonzalo <gmr57711@gmail.com>
2025-09-24 15:53:57 -0300

    no puedo creer si funciona

--- index.py
+++ index.py
@@ -1,22 +1,52 @@
-from app import crear_app
-from utils.db import db
-import config
+#from app import crear_app
+#from utils.db import db
+#import config
+#import os
+
+#app = crear_app()
+
+#if __name__ == "__main__":
+    
+#    with app.app_context():
+#        db.create_all()
+#
+#    port = int(os.environ.get("PORT", 5000))
+#    debug_mode = os.environ.get("FLASK_ENV") == "development"
+#    
+#    app.run(
+#        host="0.0.0.0",
+#        port=port,
+#        debug=debug_mode
+#        )
+
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import DATABASE_URI
 import os
 
-app = crear_app()
+db = SQLAlchemy()  # solo la declaración
+
+def create_app():
+    app = Flask(__name__)
+
+    app.secret_key = "secretkey"
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from routes.items import items
+    app.register_blueprint(items)
+
+    return app
+
 
 if __name__ == "__main__":
-    
+    app = create_app()
     with app.app_context():
         db.create_all()
 
     port = int(os.environ.get("PORT", 5000))
     debug_mode = os.environ.get("FLASK_ENV") == "development"
-    
-    app.run(
-        host="0.0.0.0",
-        port=port,
-        debug=debug_mode
-        )
-
-
+    app.run(host="0.0.0.0", port=port, debug=debug_mode)
