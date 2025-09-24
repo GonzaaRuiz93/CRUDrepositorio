ef73a8620c6ecb09df46f39a051bdb0cdf8ebfe3
Gonzalo <gmr57711@gmail.com>
2025-09-24 11:44:18 -0300

    modificando index

--- index.py
+++ index.py
@@ -1,26 +1,7 @@
-#from app import crear_app
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from config import DATABASE_URI
-#from utils.db import db
+from app import crear_app
+from utils.db import db
 import config
 import os
-
-db = SQLAlchemy()
-
-def crear_app():
-    app = Flask(__name__)
-
-    app.secret_key = "secretkey"
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db.init_app(app)
-
-    from routes.items import items
-    app.register_blueprint(items)
-
-    return app
 
 app = crear_app()
 
