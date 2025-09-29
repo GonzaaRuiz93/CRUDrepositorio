0becb860c8201940c125475d912b03a8a8019e17
Gonzalo <gmr57711@gmail.com>
2025-09-28 21:43:35 -0300

    configurando nueva base de datos

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ Host_BD = os.environ['Host_BD']
 Puerto_BD = os.environ['Puerto_BD']
 Nombre_BD = os.environ['Nombre_BD']
 
+
 # Concatenamos todo y lo pasamos a SQLAlchemy
 DATABASE_URI = f"{Tipo_BD}://{Usuario_BD}:{Password_BD}@{Host_BD}:{Puerto_BD}/{Nombre_BD}"
