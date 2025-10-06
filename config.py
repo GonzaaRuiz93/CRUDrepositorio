79fca0e91a5f533af239e52f026e1bde0ddb7757
GonzaaRuiz93 <gmr57711@gmail.com>
2025-10-06 13:02:31 -0300

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,8 @@ Host_BD = os.environ['Host_BD']
 Puerto_BD = os.environ['Puerto_BD']
 Nombre_BD = os.environ['Nombre_BD']
 
+DATABASE_URI = os.environ['DATABASE_URL']
 
 # Concatenamos todo y lo pasamos a SQLAlchemy
-DATABASE_URI = f"{Tipo_BD}://{Usuario_BD}:{Password_BD}@{Host_BD}:{Puerto_BD}/{Nombre_BD}"
+# DATABASE_URI = f"{Tipo_BD}://{Usuario_BD}:{Password_BD}@{Host_BD}:{Puerto_BD}/{Nombre_BD}"
 
